26c825aa8bca68650e1fc6941712a93625d88eef
kiback2da <kiback2da@steconsulting.co.kr>
2019-10-30 22:17:50 +0900

    숙제 + 토스트

---
+++ app/src/main/java/com/tjoeun/a20191030_homeworkadd/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.tjoeun.a20191030_homeworkadd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        actBtn.setOnClickListener {
+            var user = actId.text.toString()
+            var password = actPass.text.toString()
+
+            Log.d("로그",user)
+            Log.d("로그",password)
+
+            if (user == "Admin" && password == "1234") {
+                Toast.makeText(this, "관리자로 로그인 합니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "아이디 혹은 비밀번호가 잘못 되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        actBtn.setOnLongClickListener{
+            var user = actId.text.toString()
+            var password = actPass.text.toString()
+
+            var message = "로그인한 아이디 : "+user+" 비밀번호 : "+password+" 입니다"
+            Log.d("로그",user)
+            Log.d("로그",password)
+
+
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            return@setOnLongClickListener false
+        }
+    }
+}
