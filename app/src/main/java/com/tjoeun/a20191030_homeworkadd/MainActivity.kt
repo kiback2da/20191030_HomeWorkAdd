bec23f9d8e973e01e8dd5601c2b134012e1e7808
kiback2da <kiback2da@steconsulting.co.kr>
2019-10-31 20:04:58 +0900

    조건문을 활용한 분기처리   - Kotlin에서의 ==과 ===의 차리 설명

--- app/src/main/java/com/tjoeun/a20191030_homeworkadd/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191030_homeworkadd/MainActivity.kt
@@ -19,7 +19,10 @@ class MainActivity : AppCompatActivity() {
             Log.d("로그",user)
             Log.d("로그",password)
 
-            if (user == "Admin" && password == "1234") {
+
+            //Kotiln은 == 연산자 자체가 찾아가서 내용물을 확인하도록 되어 있다
+            //Kotlin은 === 연산자도 지원 (JAVA의 ==과 동일한 기능, 포인터 주소 비교)
+            if ("Admin".equals(user) && password == "1234") {
                 Toast.makeText(this, "관리자로 로그인 합니다.", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this, "아이디 혹은 비밀번호가 잘못 되었습니다.", Toast.LENGTH_SHORT).show()
